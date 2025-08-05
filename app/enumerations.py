4f18d04dc9c3e8fd56cb6e99d3aab4c4c286ced6
Azhar-ELBAGHAZAOUI <azhar.elbaghazaoui@gmail.com>
2025-08-05 14:33:35 +0200

    création de la base avec faker et implémentation des enumerations

--- app/enumerations.py
+++ app/enumerations.py
@@ -4,3 +4,20 @@ class Role(str, Enum):
     CLIENT = "client"
     EMPLOYEE = "employée"
     ADMIN = "admin"
+    
+class Status(str, Enum) : 
+    EN_PREPARATION = "En préparation"
+    PRETE = "Prete"
+    SERVIE = "Servie"
+    
+class StatusDelivery(str, Enum) : 
+    EN_COURS= "En cours"
+    DELIVREE = "Délivrée"
+    
+class Category(str, Enum) : 
+    ENTREE = "Entrée"
+    PLAT_PRINCIPAL = "Plat principal"
+    DESSERT = "Dessert"
+    BOISSON = "Boisson"
+    SNACK = "Snack"
+    MENU_ENFANT = "Menu enfant"
