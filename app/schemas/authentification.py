c7e2244b1667e36c15aa0f4fc7248c70a8ab7722
CVincent27 <cardon.vincent@hotmail.fr>
2025-08-07 14:43:03 +0200

    add schema auth (avec test)

--- app/schemas/authentification.py
+++ app/schemas/authentification.py
@@ -4,6 +4,11 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+# test
+class TokenUser(BaseModel):
+    email: str
+    role: str
+
 class AuthRequest(BaseModel):
     email: EmailStr
     password: str
