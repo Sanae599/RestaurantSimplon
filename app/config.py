7e14b6c69e6a87a42b1943c74643ff74c98538d8
Azhar-ELBAGHAZAOUI <azhar.elbaghazaoui@gmail.com>
2025-08-12 14:51:25 +0200

    Ajout partie docker

--- app/config.py
+++ app/config.py
@@ -2,3 +2,10 @@ SECRET_KEY = "ta_clef_secrète_super_sécurisée"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+import os
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+)
