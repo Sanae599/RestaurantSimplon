39a7fe2aa83718a9deb0a212a366d3fa088c7025
Sanae599 <sanae.almassati@outlook.com>
2025-08-14 14:22:36 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "Run Alembic migration..."
 alembic upgrade head  # direct, pas docker exec
 
-#echo "Main database..."
-#python main.py  # direct, pas docker exec
-
 echo "Start Uvicorn server..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
