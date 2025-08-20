f101a03362e2a3e17749d57f5edca393da25dac3
Sanae_almassati <sanae.almassati@outlook.com>
2025-08-20 10:57:17 +0200

    Update test_models_product.py

--- tests/unit/models/test_models_product.py
+++ tests/unit/models/test_models_product.py
@@ -13,12 +13,3 @@ def test_product_valid_enum():
         stock=20,
     )
     assert isinstance(product.created_at, datetime)
-def test_product_type_invalide():
-    """Vérifie qu'un type incorrect lève une erreur."""
-    with pytest.raises(ValidationError):
-        Product(
-            name=123,           # doit être str
-            unit_price="10",    # doit être float
-            category=145,
-            stock="abc"         # doit être int
-        )
